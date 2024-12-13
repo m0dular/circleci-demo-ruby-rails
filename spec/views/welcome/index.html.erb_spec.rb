77dd611822651911a3bb08889769d9151cdc31d5
Adrian Parreiras Horta <sensei.loafage@gmail.com>
2024-12-12 16:20:06 -0800

    Rubocop

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'welcome/index.html.erb', type: :view do
-  it "has the correct button" do
+  it 'has the correct button' do
     render
 
     expect(rendered).to match 'This is some text'
